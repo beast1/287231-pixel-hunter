35a8ff7643f737b9f141a9a1b39bf61d0338c53a
Keks <keks@htmlacademy.ru>
2017-10-29 22:17:56 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -27,8 +27,8 @@ const getHeader = (data) => {
 
   if (typeof data === `undefined`) {
     return headerDefault;
-  } else {
-    return `${headerDefault}
+  }
+  return `${headerDefault}
     <h1 class="game__timer">${data.time}</h1>
     <div class="game__lives">
       ${new Array(3 - data.lives)
@@ -38,7 +38,7 @@ const getHeader = (data) => {
       .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">`)
       .join(``)}
       </div>`;
-  }
+
 };
 
 const footer = `<footer class="footer">
@@ -52,230 +52,231 @@ const footer = `<footer class="footer">
     </div>
   </footer>`;
 
-const LIFE_WORTH = 50;
-const MAX_LIFES = 3;
-const MAX_ANSWERS_LENGTH = 10;
-const AnswerTypes = {
-  FAST: `fast`,
-  SLOW: `slow`,
-  CORRECT: `correct`,
-  WRONG: `wrong`
-};
-
-const ImageTypes = {
-  PAINT: `paint`,
-  PHOTO: `photo`
-};
-
-const initialState = {
-  level: 0,
-  lives: 3,
-  time: 30
-};
-
-const levels = [
-  {
-    description: `Найдите рисунок среди изображений`,
-    type: `TRIPLE`,
-    expect: ImageTypes.PAINT,
-    options: new Set([
-      {
-        type: ImageTypes.PHOTO,
-        image: `http://i.imgur.com/1KegWPz.jpg`
-      },
-      {
-        type: ImageTypes.PHOTO,
-        image: `https://i.imgur.com/DiHM5Zb.jpg`
-      },
-      {
-        type: ImageTypes.PAINT,
-        image: `https://k42.kn3.net/D2F0370D6.jpg`
-      },
-    ])
-  },
-  {
-    description: `Угадайте для каждого изображения фото или рисунок?`,
-    type: `DOUBLE`,
-    options: new Set([
-      {
-        type: ImageTypes.PAINT,
-        image: `http://www.fresher.ru/manager_content/images/fotorealistichnye-kartiny-italyanca-mikele-del-kampo/big/18.jpg`
-      },
-      {
-        type: ImageTypes.PHOTO,
-        image: `http://www.kartinki.me/images/201312/kartinki.me_16611.jpg`
-      }
-    ])
-  },
-  {
-    description: `Угадай, фото или рисунок?`,
-    type: `SINGLE`,
-    options: new Set([
-      {
-        type: ImageTypes.PHOTO,
-        image: `http://www.fresher.ru/manager_content/images/fotorealistichnye-kartiny-italyanca-mikele-del-kampo/big/18.jpg`
-      }
-    ])
-  },
-  {
-    description: `Угадайте для каждого изображения фото или рисунок?`,
-    type: `DOUBLE`,
-    options: new Set([
-      {
-        type: ImageTypes.PAINT,
-        image: `https://k32.kn3.net/5C7060EC5.jpg`
-      },
-      {
-        type: ImageTypes.PHOTO,
-        image: `http://i.imgur.com/DKR1HtB.jpg`
-      },
-    ])
-  },
-  {
-    description: `Угадай, фото или рисунок?`,
-    type: `SINGLE`,
-    options: new Set([
-      {
-        type: ImageTypes.PAINT,
-        image: `https://k42.kn3.net/CF42609C8.jpg`
-      }
-    ])
-  },
-  {
-    description: `Угадайте для каждого изображения фото или рисунок?`,
-    type: `DOUBLE`,
-    options: new Set([
-      {
-        type: ImageTypes.PAINT,
-        image: `https://k42.kn3.net/CF42609C8.jpg`
-      },
-      {
-        type: ImageTypes.PHOTO,
-        image: `http://i.imgur.com/1KegWPz.jpg`
-      },
-    ])
-  },
-  {
-    description: `Угадайте для каждого изображения фото или рисунок?`,
-    type: `DOUBLE`,
-    options: new Set([
-      {
-        type: ImageTypes.PAINT,
-        image: `https://k42.kn3.net/CF42609C8.jpg`
-      },
-      {
-        type: ImageTypes.PHOTO,
-        image: `http://i.imgur.com/1KegWPz.jpg`
-      },
-    ])
-  },
-  {
-    description: `Угадайте для каждого изображения фото или рисунок?`,
-    type: `DOUBLE`,
-    options: new Set([
-      {
-        type: ImageTypes.PAINT,
-        image: `https://k42.kn3.net/CF42609C8.jpg`
-      },
-      {
-        type: ImageTypes.PHOTO,
-        image: `http://i.imgur.com/1KegWPz.jpg`
-      },
-    ])
-  },
-  {
-    description: `Угадайте для каждого изображения фото или рисунок?`,
-    type: `DOUBLE`,
-    options: new Set([
-      {
-        type: ImageTypes.PAINT,
-        image: `https://k42.kn3.net/CF42609C8.jpg`
-      },
-      {
-        type: ImageTypes.PHOTO,
-        image: `http://i.imgur.com/1KegWPz.jpg`
-      },
-    ])
-  },
-  {
-    description: `Угадайте для каждого изображения фото или рисунок?`,
-    type: `DOUBLE`,
-    expect: ``,
-    options: new Set([
-      {
-        type: ImageTypes.PAINT,
-        image: `https://k42.kn3.net/CF42609C8.jpg`
-      },
-      {
-        type: ImageTypes.PHOTO,
-        image: `http://i.imgur.com/1KegWPz.jpg`
-      },
-    ])
-  },
-];
-
-const game = {
-  state: initialState,
-  history: [],
-  levels
-};
-
-const PointsForAnswers = {
-  [AnswerTypes.FAST]: 150,
-  [AnswerTypes.SLOW]: 50,
-  [AnswerTypes.CORRECT]: 100,
-  [AnswerTypes.WRONG]: 0
-};
-
-const countScore = (answers, lifes) => {
-  if (answers.length < MAX_ANSWERS_LENGTH || lifes < 0) {
-    return -1;
-  }
-
-  if (answers.filter((it) => it === AnswerTypes.WRONG).length !== (MAX_LIFES - lifes)) {
-    throw new Error(`impossible input combination`);
-  }
-
-  const initialValue = lifes * LIFE_WORTH;
-
-  return answers.reduce((sum, answer) => {
-    if (typeof PointsForAnswers[answer] !== `number`) {
-      throw new Error(`wrong answer type`);
-    }
-
-    return sum + PointsForAnswers[answer];
-  }, initialValue);
-};
-
-const MAX_LEVELS = 10;
-
 const getStats = (gameHistory) => {
   return `
       <ul class="stats">
         ${gameHistory
       .map((it) =>
         `<li class="stats__result stats__result--${it}"></li>`)
-      .concat(new Array(MAX_LEVELS - gameHistory.length)
+      .concat(new Array(MAX_ANSWERS_LENGTH - gameHistory.length)
           .fill(`<li class="stats__result stats__result--unknown"></li>`))
       .join(``)}
       </ul>`;
 };
 
-const LevelTypes = {
-  DOUBLE: `game__content`,
-  SINGLE: `game__content game__content--wide`,
-  TRIPLE: `game__content game__content--triple`
+const MAX_ANSWERS_LENGTH = 10;
+const AnswerType = {
+  FAST: `fast`,
+  SLOW: `slow`,
+  CORRECT: `correct`,
+  WRONG: `wrong`
 };
 
-const getOptions = (options, levelType) => {
-  const optionsSet = Array.from(options);
-  return (levelType === `TRIPLE`
-    ? optionsSet.map((it) =>
-      `<div class="game__option">
+const LevelType = {
+  SINGLE: `single`,
+  DOUBLE: `double`,
+  TRIPLE: `triple`
+};
+
+const ImageType = {
+  PAINT: `paint`,
+  PHOTO: `photo`
+};
+
+const initialState = () => {
+  return {
+    level: 0,
+    lives: 3,
+    time: 30
+  };
+};
+
+const initialHistory = () => {
+  return [];
+};
+
+const levels = [
+  {
+    description: `Найдите рисунок среди изображений`,
+    type: LevelType.TRIPLE,
+    expect: ImageType.PAINT,
+    options: [
+      {
+        type: ImageType.PHOTO,
+        image: `http://i.imgur.com/1KegWPz.jpg`
+      },
+      {
+        type: ImageType.PHOTO,
+        image: `https://i.imgur.com/DiHM5Zb.jpg`
+      },
+      {
+        type: ImageType.PAINT,
+        image: `https://k42.kn3.net/D2F0370D6.jpg`
+      },
+    ]
+  },
+  {
+    description: `Угадайте для каждого изображения фото или рисунок?`,
+    type: LevelType.DOUBLE,
+    options: [
+      {
+        type: ImageType.PAINT,
+        image: `http://www.fresher.ru/manager_content/images/fotorealistichnye-kartiny-italyanca-mikele-del-kampo/big/18.jpg`
+      },
+      {
+        type: ImageType.PHOTO,
+        image: `http://www.kartinki.me/images/201312/kartinki.me_16611.jpg`
+      }
+    ]
+  },
+  {
+    description: `Угадай, фото или рисунок?`,
+    type: LevelType.SINGLE,
+    options: [
+      {
+        type: ImageType.PHOTO,
+        image: `http://www.fresher.ru/manager_content/images/fotorealistichnye-kartiny-italyanca-mikele-del-kampo/big/18.jpg`
+      }
+    ]
+  },
+  {
+    description: `Угадайте для каждого изображения фото или рисунок?`,
+    type: LevelType.DOUBLE,
+    options: [
+      {
+        type: ImageType.PAINT,
+        image: `https://k32.kn3.net/5C7060EC5.jpg`
+      },
+      {
+        type: ImageType.PHOTO,
+        image: `http://i.imgur.com/DKR1HtB.jpg`
+      },
+    ]
+  },
+  {
+    description: `Угадай, фото или рисунок?`,
+    type: LevelType.SINGLE,
+    options: [
+      {
+        type: ImageType.PAINT,
+        image: `https://k42.kn3.net/CF42609C8.jpg`
+      }
+    ]
+  },
+  {
+    description: `Угадайте для каждого изображения фото или рисунок?`,
+    type: LevelType.DOUBLE,
+    options: [
+      {
+        type: ImageType.PAINT,
+        image: `https://k42.kn3.net/CF42609C8.jpg`
+      },
+      {
+        type: ImageType.PHOTO,
+        image: `http://i.imgur.com/1KegWPz.jpg`
+      },
+    ]
+  },
+  {
+    description: `Угадайте для каждого изображения фото или рисунок?`,
+    type: LevelType.DOUBLE,
+    options: [
+      {
+        type: ImageType.PAINT,
+        image: `https://k42.kn3.net/CF42609C8.jpg`
+      },
+      {
+        type: ImageType.PHOTO,
+        image: `http://i.imgur.com/1KegWPz.jpg`
+      },
+    ]
+  },
+  {
+    description: `Угадайте для каждого изображения фото или рисунок?`,
+    type: LevelType.DOUBLE,
+    options: [
+      {
+        type: ImageType.PAINT,
+        image: `https://k42.kn3.net/CF42609C8.jpg`
+      },
+      {
+        type: ImageType.PHOTO,
+        image: `http://i.imgur.com/1KegWPz.jpg`
+      },
+    ]
+  },
+  {
+    description: `Угадайте для каждого изображения фото или рисунок?`,
+    type: LevelType.DOUBLE,
+    options: [
+      {
+        type: ImageType.PAINT,
+        image: `https://k42.kn3.net/CF42609C8.jpg`
+      },
+      {
+        type: ImageType.PHOTO,
+        image: `http://i.imgur.com/1KegWPz.jpg`
+      },
+    ]
+  },
+  {
+    description: `Угадайте для каждого изображения фото или рисунок?`,
+    type: LevelType.DOUBLE,
+    expect: ``,
+    options: [
+      {
+        type: ImageType.PAINT,
+        image: `https://k42.kn3.net/CF42609C8.jpg`
+      },
+      {
+        type: ImageType.PHOTO,
+        image: `http://i.imgur.com/1KegWPz.jpg`
+      },
+    ]
+  },
+];
+
+const game = {
+  state: initialState(),
+  history: initialHistory(),
+  levels
+};
+
+
+
+
+
+const levelChange = (gameData, condition, cb) => {
+  if (condition) {
+    gameData.history.push(AnswerType.CORRECT);
+  } else {
+    gameData.state.lives -= 1;
+    gameData.history.push(AnswerType.WRONG);
+  }
+
+  if (gameData.state.level === 9 || gameData.state.lives < 0) {
+    showScreen(getStats(gameData));
+  } else {
+    gameData.state.level += 1;
+    showScreen(cb(gameData));
+  }
+};
+
+const LevelClass = {
+  [LevelType.DOUBLE]: `game__content`,
+  [LevelType.SINGLE]: `game__content game__content--wide`,
+  [LevelType.TRIPLE]: `game__content game__content--triple`
+};
+
+const getOptions = (options, levelType) =>
+  options.map((it, i) => {
+    if (levelType === LevelType.TRIPLE) {
+      return `<div class="game__option">
         <img src="${it.image}" alt="Option" width="304" height="455">
-      </div>`).join(``)
-    : optionsSet.map((it, i) =>
-      `<div class="game__option">
+      </div>`;
+    }
+
+    return `<div class="game__option">
         <img src="${it.image}" alt="Option">
         <label class="game__answer  game__answer--photo">
           <input name="question${i + 1}" type="radio" value="photo">
@@ -285,115 +286,22 @@ const getOptions = (options, levelType) => {
           <input name="question${i + 1}" type="radio" value="paint">
           <span>Рисунок</span>
         </label>
-      </div>`).join(``));
-};
+      </div>`;
+  }).join(``);
 
-const getLevel = (levelData) => {
-  return `<p class="game__task">${levelData.description}</p>
-    <form class="${LevelTypes[levelData.type]}">
+const getLevel = (levelData) =>
+  `<p class="game__task">${levelData.description}</p>
+    <form class="${LevelClass[levelData.type]}">
       ${getOptions(levelData.options, levelData.type)}      
     </form>`;
-};
 
-const Results = {
-  VICTORY: `Победа!`,
-  LOSE: `Поражение`
-};
-
-const EXTRA_POINTS = 50;
-const BASE_POINTS = 100;
-
-const getResult = (game$$1) => {
-  const RESULT = countScore(game$$1.history, game$$1.state.lives);
-  const STATS_BAR = getStats(game$$1.history);
-  const CORRECT_ANSWERS = game$$1.history.filter((it) =>
-    it !== AnswerTypes.WRONG).length;
-  const FAST_ANSWERS = game$$1.history.filter((it) =>
-    it === AnswerTypes.FAST).length;
-  const SLOW_ANSWERS = game$$1.history.filter((it) =>
-    it === AnswerTypes.SLOW).length;
-  const tableContent = [];
-
-  if (RESULT < 0) {
-    tableContent.push(`<tr>
-        <td class="result__number"></td>
-        <td>${STATS_BAR}</td>
-        <td class="result__total"></td>
-        <td class="result__total result__total--final">fail</td>
-      </tr>`);
-  } else {
-    tableContent.push(`<tr>
-        <td class="result__number"></td>
-        <td colspan="2">
-          ${STATS_BAR}
-        </td>
-        <td class="result__points">×&nbsp;${BASE_POINTS}</td>
-        <td class="result__total">${BASE_POINTS * CORRECT_ANSWERS}</td>
-        </tr>`);
-
-    if (FAST_ANSWERS > 0) {
-      tableContent.push(`<tr>
-        <td></td>
-        <td class="result__extra">Бонус за скорость:</td>
-        <td class="result__extra">${FAST_ANSWERS}&nbsp;<span class="stats__result stats__result--fast"></span></td>
-        <td class="result__points">×&nbsp;${EXTRA_POINTS}</td>
-        <td class="result__total">${FAST_ANSWERS * EXTRA_POINTS}</td>
-      </tr>`);
-    }
-
-    if (game$$1.state.lives > 0) {
-      tableContent.push(`<tr>
-        <td></td>
-        <td class="result__extra">Бонус за жизни:</td>
-        <td class="result__extra">${game$$1.state.lives}&nbsp;<span class="stats__result stats__result--alive"></span></td>
-        <td class="result__points">×&nbsp;${EXTRA_POINTS}</td>
-        <td class="result__total">${game$$1.state.lives * EXTRA_POINTS}</td>
-      </tr>`);
-    }
-
-    if (SLOW_ANSWERS > 0) {
-      tableContent.push(`<tr>
-        <td></td>
-        <td class="result__extra">Штраф за медлительность:</td>
-        <td class="result__extra">${SLOW_ANSWERS}&nbsp;<span class="stats__result stats__result--slow"></span></td>
-        <td class="result__points">×&nbsp;${EXTRA_POINTS}</td>
-        <td class="result__total">-${SLOW_ANSWERS * EXTRA_POINTS}</td>
-      </tr>`);
-    }
-
-    tableContent.push(`<tr>
-        <td colspan="5" class="result__total  result__total--final">${RESULT}</td>
-      </tr>`);
-  }
-
-  return `<div class="result">
-    <h1>${RESULT < 0 ? Results.LOSE : Results.VICTORY}</h1>
-    <table class="result__table">${tableContent.join(``)}</table>
-  </div>`;
-};
-
-const getStatsElement = (gameData) => {
-  const layout = `${getHeader()}
-  ${getResult(gameData)}
-  ${footer}`;
-
-  const stats = getElementFromTemplate(layout);
-  const back = stats.querySelector(`.back`);
-
-  back.addEventListener(`click`, () => {
-    showScreen(greetingElement);
-  });
-
-  return stats;
-};
-
-const getLayout = (gameData) => {
+const getElement = (gameData) => {
   const level = gameData.levels[gameData.state.level];
-  const options = Array.from(level.options);
+  const options = level.options;
   const answers = options.map((it) =>
     it.type);
 
-  let element = getElementFromTemplate(`${getHeader(gameData.state)}
+  const element = getElementFromTemplate(`${getHeader(gameData.state)}
   <div class="game">
     ${getLevel(level)}
     <div class="stats">
@@ -407,65 +315,34 @@ const getLayout = (gameData) => {
   const back = element.querySelector(`.back`);
 
   back.addEventListener(`click`, () => {
+    // if (confirm(`Вы уверены? Текущий прогресс будет потерян`)) {
     showScreen(greetingElement);
+    // }
   });
 
-  if (level.type === `TRIPLE`) {
+  if (level.type === LevelType.TRIPLE) {
     form.addEventListener(`click`, (evt) => {
       if (evt.target.classList.contains(`game__option`)) {
-        if (level.expect === answers[Array.from(evt.target.parentNode.children).indexOf(evt.target)]) {
-          gameData.history.push(AnswerTypes.CORRECT);
-        } else {
-          gameData.state.lives -= 1;
-          gameData.history.push(AnswerTypes.WRONG);
-        }
+        const currentAnswer = answers[Array.from(evt.target.parentNode.children).indexOf(evt.target)];
+        const condition = level.expect === currentAnswer;
 
-        if (gameData.state.level === 10 || gameData.state.lives < 0) {
-          showScreen(getStatsElement(gameData));
-        } else {
-          game.state.level += 1;
-          element = getLayout(game);
-          showScreen(element);
-        }
+        levelChange(gameData, condition, getElement);
       }
     });
   } else {
     form.addEventListener(`change`, () => {
-      const radios = form.querySelectorAll(`input[type="radio"]:checked`);
+      const radios = Array.from(form.querySelectorAll(`input[type="radio"]:checked`));
+      const condition = radios.every((it, i) =>
+        it.value === options[i].type);
 
       if (radios.length === fields.length) {
-        let answer = 0;
-
-        radios.forEach((it, i) => {
-          if (it.value === options[i].type) {
-            answer++;
-          }
-        });
-
-        if (answer === radios.length) {
-          gameData.history.push(AnswerTypes.CORRECT);
-        } else {
-          gameData.state.lives -= 1;
-          gameData.history.push(AnswerTypes.WRONG);
-        }
-
-        if (gameData.state.level === 9 || gameData.state.lives < 0) {
-          showScreen(getStatsElement(gameData));
-        } else {
-          game.state.level += 1;
-          element = getLayout(gameData);
-          showScreen(element);
-        }
+        levelChange(gameData, condition, getElement);
       }
     });
   }
 
   return element;
 };
-
-let gameElement = getLayout(game);
-
-showScreen(gameElement);
 
 const layout$2 = `${getHeader()}
   <div class="rules">
@@ -502,7 +379,7 @@ input.addEventListener(`change`, () => {
 });
 
 button.addEventListener(`click`, () => {
-  showScreen(gameElement);
+  showScreen(getElement(game));
 });
 
 const layout$1 = `<div class="greeting central--blur">
