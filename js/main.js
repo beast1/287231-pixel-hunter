7d8b7229b077fe934aee797c8606b12ce00e8b13
Egor <beast.loby@gmail.com>
2018-02-16 04:14:57 +0200

    m1t2:переключение_экранов

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-const appendWindow = function(template) {
-  
-};
+const windows = Array.from(document.querySelectorAll(`template`));
+let currentWindow = 0;
 
-const switchWindow = function(WINDOW_NUM) {
+const switchWindow = (WINDOW_NUM) => {
   const parent = document.querySelector(`main.central`);
   // Почистить содержимое
   parent.innerHTML = ``;
+  // Выбрать новый экран
+  const window = windows[WINDOW_NUM].content.cloneNode(true);
   // Вставить новый экран
-  parent.appendWindow(WINDOW_NUM);
+  parent.appendChild(window);
 };
+
+document.addEventListener(`keyup`, (e) => {
+  if (e.keyCode === 39 && e.altKey === true) {
+    if (currentWindow < windows.length - 1) {
+      currentWindow = currentWindow + 1;
+    }
+  } else if (e.keyCode === 37 && e.altKey === true) {
+    if (currentWindow !== 0) {
+      currentWindow = currentWindow - 1;
+    }
+  }
+
+  switchWindow(currentWindow);
+});
