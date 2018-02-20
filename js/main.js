a2144059cade17aab241cd1b3d2e6930f4a6334d
Egor <beast.loby@gmail.com>
2018-02-20 19:49:29 +0200

    промежуточный

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
-const windows = Array.from(document.querySelectorAll(`template`));
-let currentWindow = 0;
+import getGreeting from "./greeting";
 
-const switchWindow = (WINDOW_NUM) => {
-  const parent = document.querySelector(`main.central`);
-  // Почистить содержимое
-  parent.innerHTML = ``;
-  // Выбрать новый экран
-  const window = windows[WINDOW_NUM].content.cloneNode(true);
-  // Вставить новый экран
-  parent.appendChild(window);
-};
+getGreeting();
 
-document.addEventListener(`keyup`, (e) => {
-  if (e.keyCode === 39 && e.altKey === true) {
-    if (currentWindow < windows.length - 1) {
-      currentWindow = currentWindow + 1;
-    }
-  } else if (e.keyCode === 37 && e.altKey === true) {
-    if (currentWindow !== 0) {
-      currentWindow = currentWindow - 1;
-    }
-  }
-
-  switchWindow(currentWindow);
-});
+//const windows = Array.from(document.querySelectorAll(`template`));
+//let currentWindow = 0;
+//
+//const switchWindow = (WINDOW_NUM) => {
+//  const parent = document.querySelector(`main.central`);
+//  // Почистить содержимое
+//  parent.innerHTML = ``;
+//  // Выбрать новый экран
+//  const window = windows[WINDOW_NUM].content.cloneNode(true);
+//  // Вставить новый экран
+//  parent.appendChild(window);
+//};
+//
+//document.addEventListener(`keyup`, (e) => {
+//  if (e.keyCode === 39 && e.altKey === true) {
+//    if (currentWindow < windows.length - 1) {
+//      currentWindow = currentWindow + 1;
+//    }
+//  } else if (e.keyCode === 37 && e.altKey === true) {
+//    if (currentWindow !== 0) {
+//      currentWindow = currentWindow - 1;
+//    }
+//  }
+//
+//  switchWindow(currentWindow);
+//});
